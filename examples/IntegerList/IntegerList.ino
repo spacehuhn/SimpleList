824a5327719b53a4c610c6519e37c2b109c44631
Stefan Kremser <stef.1497@gmail.com>
2018-06-07 16:20:31 +0200

    Improved Example

--- examples/IntegerList/IntegerList.ino
+++ examples/IntegerList/IntegerList.ino
@@ -1,105 +1,117 @@
- /*
-  ===========================================
-       Copyright (c) 2018 Stefan Kremser
-              github.com/spacehuhn
-  ===========================================
-*/
+/*
+   ===========================================
+      Copyright (c) 2018 Stefan Kremser
+             github.com/spacehuhn
+   ===========================================
+ */
 
-#include "SimpleList.h" 
- 
-SimpleList<int>* list; 
- 
-void printList(){ 
-  for(int i=0;i<list->size();i++){ 
-    Serial.print(list->get(i));   
-    if(i<list->size()-1) Serial.print("->"); 
-  } 
-  Serial.println(); 
-} 
- 
-void setup() { 
-  Serial.begin(115200); 
-  delay(2000); 
- 
-  list = new SimpleList<int>; 
-  list->add(1); 
-  list->add(0); 
-  list->add(7); 
-  list->add(1); 
-  list->add(6); 
-  list->add(16); 
-   
-  printList(); 
-  // => 1 -> 0 -> 7 -> 1 -> 6 -> 16 
-   
-  list->remove(5); // remove node at index 5, alternative: removeNode(listSize-1); 
-  list->remove(0); // remove node at index 0 
-  printList(); 
-  // => 0 -> 7 -> 1 -> 6 
-   
-  list->add(4); 
-  printList(); 
-  // => 0 -> 7 -> 1 -> 6 -> 4 
-   
-  list->replace(0,3); 
-  printList(); 
-  // => 3 -> 7 -> 1 -> 6 -> 4 
-   
-  list->replace(list->search([](int &a) -> bool{ 
-    return a == 7; 
-  }),2); 
-  printList(); 
-  // => 3 -> 2 -> 1 -> 6 -> 4 
-   
-  list->sort([](int &a, int &b) -> bool { 
-    return a > b; 
-  }); 
-  printList(); 
-  // => 1 -> 2 -> 3 -> 4 -> 6 
- 
-  Serial.println(list->binSearch([](int &a) -> int{ 
-    if(a == 1) return 0; 
-    if(a < 1) return 1; 
-    if(a > 1) return -1;   
-  })); 
- 
-  Serial.println(list->binSearch([](int &a) -> int{ 
-    if(a == 6) return 0; 
-    if(a < 6) return 1; 
-    if(a > 6) return -1;   
-  })); 
- 
-  Serial.println(list->binSearch([](int &a) -> int{ 
-    if(a == 3) return 0; 
-    if(a < 3) return 1; 
-    if(a > 3) return -1;   
-  })); 
-   
-  list->add(list->size()-2, list->size()); 
-  printList(); 
-  // => 1 -> 2 -> 3 -> 4 -> 5 -> 6 
-   
-  list->clear(); 
-  // => Empty list :( 
-  printList(); 
- 
-  for(int i=0;i<10;i++) 
-    list->add(random(0,3)); 
-   
-  printList(); 
-   
-  Serial.println(list->count([](int &a) -> bool{ 
-    return a == 0; 
-  })); 
- 
-  Serial.println("removed "+(String)list->shift()); 
-  printList(); 
-   
-  Serial.println("removed "+(String)list->pop()); 
-  printList(); 
-} 
- 
-void loop() { 
-  //list.add(int(1)); 
+#include "SimpleList.h"
+
+SimpleList<int>* list;
+
+void printList() {
+    Serial.print("=>");
+
+    for (int i = 0; i < list->size(); i++) {
+        Serial.print(list->get(i));
+        Serial.print("->");
+    }
+    Serial.println("//");
 }
- 
+
+void setup() {
+    Serial.begin(115200);
+    delay(200);
+
+    // create new list
+    list = new SimpleList<int>;
+
+    // add a bunch of numbers
+    list->add(1);
+    list->add(0);
+    list->add(7);
+    list->add(1);
+    list->add(6);
+    list->add(16);
+
+    // print out the list
+    printList();
+    // => 1 -> 0 -> 7 -> 1 -> 6 -> 16 -> //
+
+    // remove number 6 and 1
+    list->remove(5); // remove node at index 5, alternative: removeNode(listSize-1);
+    list->remove(0); // remove node at index 0
+    printList();
+    // => 0 -> 7 -> 1 -> 6 -> //
+
+    // add number 4 at the end
+    list->add(4);
+    printList();
+    // => 0 -> 7 -> 1 -> 6 -> 4 -> //
+
+    // replace number 0 with 3
+    list->replace(0, 3);
+    printList();
+    // => 3 -> 7 -> 1 -> 6 -> 4 -> //
+
+    // add a compare function to sort or search the list
+    list->compare = [](int& a, int& b) -> int {
+                        if (a < b) return -1;
+
+                        if (a == b) return 0;
+
+                        if (a > b) return 1;
+                    };
+
+    // find the number 7 and replace it with a 2
+    list->replace(list->search(7), 2);
+    printList();
+    // => 3 -> 2 -> 1 -> 6 -> 4 -> //
+
+    // sort the list
+    list->sort();
+    printList();
+    // => 1 -> 2 -> 3 -> 4 -> 6 -> //
+
+    // perform a fast binary search (only works when the list is sorted!)
+    uint32_t start = millis();
+    Serial.println("1 at index (bin-search): " + (String)list->binSearch(1));
+    Serial.println("6 at index (bin-search): " + (String)list->binSearch(6));
+    Serial.println("3 at index (bin-search): " + (String)list->binSearch(3));
+
+    // or do the slower sequential search (won't make a time difference in small lists like this)
+    Serial.println("1 at index (seq-search): " + (String)list->search(1));
+    Serial.println("6 at index (seq-search): " + (String)list->search(6));
+    Serial.println("3 at index (seq-search): " + (String)list->search(3));
+
+    // insert number 5 (which ist the list size) in the sorted list
+    // note here that insert() will try to put the object in correct spot to keep the list sorted
+    list->insert(list->size());
+    printList();
+    // => 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> //
+
+    // empty the list
+    list->clear();
+    // => Empty list :(
+    printList();
+
+    // ad random numbers
+    for (int i = 0; i < 10; i++) list->add(random(0, 9));
+    printList();
+
+    // with shift() you get and remove the first object from the list
+    Serial.println("removed first object " + (String)list->shift());
+    printList();
+
+    // with pop() you get and remove the last object from the list
+    Serial.println("removed last object " + (String)list->pop());
+    printList();
+
+    Serial.println("List sorted: " + String(list->isSorted() ? "true" : "false"));
+    printList();
+    list->sort();
+    Serial.println("List sorted: " + String(list->isSorted() ? "true" : "false"));
+    printList();
+}
+
+void loop() {}
